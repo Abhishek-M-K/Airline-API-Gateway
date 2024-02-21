68af08525f93662f7e9be0abb475da9a29927313
Abhishek-M-K <khandareabhishek98@gmail.com>
2024-02-21 21:15:31 +0530

    chore : add flightservice route

--- index.js
+++ index.js
@@ -11,26 +11,43 @@ const PORT = process.env.PORT;
 
 const limiter = rateLimit({
   windowMs: 2 * 60 * 1000,
-  max: 5,
+  max: 20,
   message: "Too many requests from this IP, please try again later",
 });
 
 app.use(morgan("combined"));
 app.use(limiter);
 
-app.use('/bookingservice', (req,res,next)=>{
-    //put it in a try catch block
-    console.log(req.headers['x-access-token']);
-    const response = axios.get('http://localhost:3001/api/v1/isUserAuthenticated', {
-        headers:{
-            'x-access-token': req.headers['x-access-token']
-        }
-    });
+app.use(
+  "/flightservice",
+  createProxyMiddleware({
+    target: "http://localhost:3036/",
+    changeOrigin: true,
+  })
+);
+
+app.use("/bookingservice", (req, res, next) => {
+  // console.log(req.headers["x-access-token"]);
+  try {
+    const response = axios.get(
+      "http://localhost:3001/api/v1/isUserAuthenticated",
+      {
+        headers: {
+          "x-access-token": req.headers["x-access-token"],
+        },
+      }
+    );
     console.log(response.data);
-    console.log('Middleware for booking service');
-    if(response.data.success) next();
-    else res.status(401).json({message: 'Unauthorized'}); 
-})
+    console.log("Middleware for booking service");
+
+    if (response.data.success) next();
+    else return res.status(401).json({ message: "Unauthorized" });
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+});
 
 app.use(
   "/bookingservice",
